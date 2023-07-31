2d1059a21957ae52d4a1a0ae9e115d142b5fb97f
emreylc <yalcnnemre@gmail.com>
2023-07-31 14:17:58 +0300

    added react slick package

--- figmaproject/src/App.tsx
+++ figmaproject/src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import HomePage from './pages/HomePage'
-
+import Header from './components/Header'
+import FirstSection from './components/FirstSection'
+import {BrowserRouter,Route,Routes} from "react-router-dom"
+import LandingPage from './pages/LandingPage'
 const App = () => {
   return (
-    <HomePage />
+    <BrowserRouter>
+        <Routes>
+          <Route path='/test' element={<HomePage/>}    />
+          <Route  path='/' element={<LandingPage/>}  />
+        </Routes>
+    </BrowserRouter>
+
   )
 }
 
